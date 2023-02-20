41021d8e1a86746904d15d73f0625001046abd0e
idugan100 <isaacd4444@gmail.com>
2023-02-20 15:18:39 -0500

    sanctum scaffolding and tile model and migration

--- tile_backend/routes/web.php
+++ tile_backend/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TokenController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('/tokens',TokenController::class);
+
