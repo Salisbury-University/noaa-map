fda26fcd08f4d57ad913123b56de72229e1eda62
idugan100 <isaacd4444@gmail.com>
2023-02-21 14:55:41 -0500

    token table setup

--- tile_backend/resources/views/home.blade.php
+++ tile_backend/resources/views/home.blade.php
@@ -5,20 +5,38 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
+                <div class="card-header">{{ __('My Tokens') }}</div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <button class="m-3 btn btn-dark">New Token</button>
+                    <table class=" m-3 table table-striped">
+                        <thead class="thead-dark">
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                            @foreach (auth()->user()->tokens as $token)
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                              </tr>
+                            @endforeach
+                          
+                        </tbody>
+                      </table>
 
-                    {{ __('You are logged in!') }}
                 </div>
-                <img  class="m-2" src="{{asset("/logo.svg")}}" alt="">
+                
             </div>
         </div>
     </div>
+
 </div>
+
+
 @endsection
