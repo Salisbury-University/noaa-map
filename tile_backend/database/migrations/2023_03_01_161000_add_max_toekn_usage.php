f600d5e10a3251df461277654fbc256f9952bb03
idugan100 <isaacd4444@gmail.com>
2023-03-01 11:55:21 -0500

    added token limits, limit reached API response and limit indicator highlighting on the token page

---
+++ tile_backend/database/migrations/2023_03_01_161000_add_max_toekn_usage.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('personal_access_tokens', function (Blueprint $table) {
+            $table->integer('max_usage')->default(100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('personal_access_tokens', function (Blueprint $table) {
+            $table->dropColumn('max_usage');
+        });
+    }
+};
--- tile_backend/resources/views/home.blade.php
+++ tile_backend/resources/views/home.blade.php
@@ -27,14 +27,15 @@
                 <div class="card-body">
                     <a href="{{route("tokens.create")}}" class="m-3 btn " style="background-color: #009688; color:white;">New Token</a>
                     
-                    <table class=" m-3 table table-striped">
+                    <table class=" m-3 table table-striped ">
                         <thead class="thead-dark">
                           <tr>
                             <th scope="col">#</th>
                             <th scope="col">Name</th>
                             <th scope="col">Created</th>
                             <th scope="col">Last Used</th>
-                            <th scope="col">Number of Uses</th>
+                            <th scope="col">Uses</th>
+                            <th scope="col">Max Uses</th>
                             <th scope="col">Actions</th>
 
 
@@ -47,7 +48,16 @@
                                 <td>{{$token->name}}</td>
                                 <td>{{$token->created_at}}</td>
                                 <td>{{$token->last_used_at}}</td>
-                                <td>{{$token->uses}}</td>
+                                <td 
+                                    @if ($token->uses>=$token->max_usage) class="table-danger"
+                                    @elseif($token->uses>.75*$token->max_usage) class="table-warning"
+                                    @endif class="table-success"
+                                    >{{$token->uses}}</td>
+                                <td 
+                                    @if ($token->uses>=$token->max_usage) class="table-danger"
+                                    @elseif($token->uses>.75*$token->max_usage) class="table-warning"
+                                    @endif class="table-success"
+                                    >{{$token->max_usage}}</td>
                                 <td>
                                     <form method="POST" action="{{route("tokens.destroy",$token->id)}}">
                                         @method('DELETE')
@@ -57,7 +67,6 @@
                                 </td>
                               </tr>
                             @endforeach
-                          
                         </tbody>
                       </table>
 
