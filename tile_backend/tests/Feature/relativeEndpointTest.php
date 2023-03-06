1dfba292e285f5a73212fc081695f424a1390e21
idugan100 <isaacd4444@gmail.com>
2023-03-06 11:56:05 -0500

    relative endpoint automated test

--- tile_backend/tests/Feature/relativeEndpointTest.php
+++ tile_backend/tests/Feature/relativeEndpointTest.php
@@ -7,22 +7,21 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class coordinateTest extends TestCase
+class relativeEndpointTest extends TestCase
 {
     /**
      * A basic feature test example.
      */
-    public function testCoordinateEndpoint(): void
+    public function testRelativeEndpoint(): void
     {
         $user = User::factory()->make();        
         $this->actingAs($user);
 
-        $response = $this->get('/api/coordinate/50/50/5');
+        $response = $this->get('/api/relative/50/50/5');
 
         $response->assertStatus(200);
         $response->assertJson([
-            "message"=>"Tile at 50, 50 with a width of 5"
+            "message"=>"Tile at 50, 50, 5"
         ]);
-        
     }
 }
