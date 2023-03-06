616244f438bcb4269f6922b67c601660e8e5b265
jmeyers5 <jmeyers5@gulls.salisbury.edu>
2023-03-06 11:16:05 -0500

    map with grid

--- my-app/main.js
+++ my-app/main.js
@@ -1,17 +1,20 @@
-import './style.css';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import Map from 'ol/Map.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
+import {OSM, TileDebug} from 'ol/source.js';
 
 const map = new Map({
-  target: 'map',
   layers: [
     new TileLayer({
-      source: new OSM()
-    })
+      source: new OSM(),
+    }),
+    new TileLayer({
+      source: new TileDebug(),
+    }),
   ],
+  target: 'map',
   view: new View({
     center: [0, 0],
-    zoom: 2
-  })
+    zoom: 1,
+  }),
 });
