5f204582e9dc2629573c85310d5b95bf85fd3d34
Bryan Yoder <bryanyoder14@gmail.com>
2023-03-06 11:49:21 -0500

    Open Layers Test commit 1

--- my-app/main.js
+++ my-app/main.js
@@ -1,17 +1,39 @@
-import './style.css';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import Map from 'ol/Map.js';
+import OSM, {ATTRIBUTION} from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
+
+const openCycleMapLayer = new TileLayer({
+  source: new OSM({
+    attributions: [
+      'All maps © <a href="https://www.opencyclemap.org/">OpenCycleMap</a>',
+      ATTRIBUTION,
+    ],
+    url:
+      'https://{a-c}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png' +
+      '?apikey=Your API key from https://www.thunderforest.com/docs/apikeys/ here',
+  }),
+});
+
+const openSeaMapLayer = new TileLayer({
+  source: new OSM({
+    attributions: [
+      'All maps © <a href="https://www.openseamap.org/">OpenSeaMap</a>',
+      ATTRIBUTION,
+    ],
+    opaque: false,
+    url: 'https://tiles.openseamap.org/seamark/{z}/{x}/{y}.png',
+  }),
+});
 
 const map = new Map({
+  layers: [new TileLayer({
+    source: new OSM(),
+  }), openSeaMapLayer],
   target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM()
-    })
-  ],
   view: new View({
-    center: [0, 0],
-    zoom: 2
-  })
+    maxZoom: 18,
+    center: [-244780.24508882355, 5986452.183179816],
+    zoom: 15,
+  }),
 });
