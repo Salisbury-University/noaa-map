dc3e81e2717cb7b5e7d770d8fdf57349e3efc773
Bryan Yoder <bryanyoder14@gmail.com>
2023-03-27 11:55:38 -0400

    Added favicon and title to HTML, and image folder

--- my-app/main.js
+++ my-app/main.js
@@ -3,17 +3,7 @@ import OSM, {ATTRIBUTION} from 'ol/source/OSM.js';
 import TileLayer from 'ol/layer/Tile.js';
 import View from 'ol/View.js';
 
-const openCycleMapLayer = new TileLayer({
-  source: new OSM({
-    attributions: [
-      'All maps © <a href="https://www.opencyclemap.org/">OpenCycleMap</a>',
-      ATTRIBUTION,
-    ],
-    url:
-      'https://{a-c}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png' +
-      '?apikey=Your API key from https://www.thunderforest.com/docs/apikeys/ here',
-  }),
-});
+
 
 const openSeaMapLayer = new TileLayer({
   source: new OSM({
@@ -26,14 +16,17 @@ const openSeaMapLayer = new TileLayer({
   }),
 });
 
+
+
 const map = new Map({
   layers: [new TileLayer({
     source: new OSM(),
-  }), openSeaMapLayer],
+  }),openSeaMapLayer],
   target: 'map',
   view: new View({
     maxZoom: 18,
     center: [-244780.24508882355, 5986452.183179816],
-    zoom: 15,
+    zoom: 16,
   }),
 });
+
