02104218418659ce5431e71538c9ad9444d48e7e
Bryan Yoder <bryanyoder14@gmail.com>
2023-04-12 11:39:17 -0400

    Made Logo transparent

--- my-app/main.js
+++ my-app/main.js
@@ -2,13 +2,14 @@ import Map from 'ol/Map.js';
 import OSM, {ATTRIBUTION} from 'ol/source/OSM.js';
 import TileLayer from 'ol/layer/Tile.js';
 import View from 'ol/View.js';
+import {FullScreen, defaults as defaultControls} from 'ol/control.js';
 
 
 
 const openSeaMapLayer = new TileLayer({
   source: new OSM({
     attributions: [
-      'All maps © <a href="https://www.openseamap.org/">OpenSeaMap</a>',
+      'All maps © <a href="https://distribution.charts.noaa.gov/ncds/index.html">NOAA</a>',
       ATTRIBUTION,
     ],
     opaque: false,
@@ -19,14 +20,16 @@ const openSeaMapLayer = new TileLayer({
 
 
 const map = new Map({
+  controls: defaultControls().extend([new FullScreen()]),
   layers: [new TileLayer({
     source: new OSM(),
   }),openSeaMapLayer],
   target: 'map',
   view: new View({
     maxZoom: 18,
-    center: [-244780.24508882355, 5986452.183179816],
-    zoom: 16,
+    center: [-8362003.0, 4956269.0],
+    zoom: 5,
   }),
 });
+
 
