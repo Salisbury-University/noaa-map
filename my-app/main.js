c312ae2f5fd259ecdc8a4b6c337cc48fb139b08b
idugan100 <isaacd4444@gmail.com>
2023-05-10 22:10:24 -0400

    added map on intial page load and refactored view js to be more modular

--- my-app/main.js
+++ my-app/main.js
@@ -7,13 +7,7 @@ import TileGrid from 'ol/tilegrid/TileGrid';
 import {FullScreen, defaults as defaultControls} from 'ol/control.js';
 
 
-// Define your tile grid
-let gridID=document.getElementById("grid_selector")
-
-
-  
-
-var tileGrid = new TileGrid({
+let tileGrid = new TileGrid({
   extent: [-20037508.34, -20037508.34, 20037508.34, 20037508.34],
   resolutions: [
     156543.033928041,
@@ -48,64 +42,89 @@ const backgroundLayer = new TileLayer({
   }),
 });
 
-gridID.addEventListener("change",(event)=>{
-map=document.getElementById("map");
-map.innerHTML="";
-console.log(event.target.value);
+//onload functionality
+let gridID=document.getElementById("grid_selector");
+render_map(gridID.value);
+
+//selection functionallity
+gridID.addEventListener("change",(event)=>{render_map(event.target.value);});
+
+
+//=============================================================================================================
+function render_map(tile_id){
+
+  clear_map();
+  let dataTileLayer=create_data_tile_layer(tile_id);
+  let map=initialize_map(dataTileLayer);  
+  display_zoom_level(map);
+  display_mouse_xyz(map);
+}
+
+//remove elements inside map
+function clear_map(){
+    let map=document.getElementById("map");
+    map.innerHTML="";
+}
 
 //Create Layer displaying API data
-var dataTileLayer = new TileLayer({
-  source: new XYZ({
-    attributions: [
-      'All map data © <a href="https://distribution.charts.noaa.gov/ncds/index.html">NOAA</a>',
-      ATTRIBUTION,
-    ],
-    opaque: false,
-    tileUrlFunction:  function(tileCoord){
-      var z = tileCoord[0];
-      var x = tileCoord[1]; 
-      var y = tileCoord[2];
-      var yFlipped = Math.pow(2, z) - y - 1; // flip the Y axis
-      var yAdjusted = yFlipped; // adjust the Y value 
-      return 'https://bathmap.net/api/relative/'+event.target.value+ z + '/' + x + '/'+yAdjusted ;
-    },
-    tileGrid : tileGrid,
-  }),
-});
+function create_data_tile_layer(tile_id){
+  let dataTileLayer = new TileLayer({
+    source: new XYZ({
+      attributions: [
+        'All map data © <a href="https://distribution.charts.noaa.gov/ncds/index.html">NOAA</a>',
+        ATTRIBUTION,
+      ],
+      opaque: false,
+      tileUrlFunction:  function(tileCoord){
+        let z = tileCoord[0];
+        let x = tileCoord[1]; 
+        let y = tileCoord[2];
+        let yFlipped = Math.pow(2, z) - y - 1; // flip the Y axis
+        let yAdjusted = yFlipped; // adjust the Y value 
+        return 'https://bathmap.net/api/relative/'+tile_id+ z + '/' + x + '/'+yAdjusted ;
+      },
+      tileGrid : tileGrid,
+    }),
+  });
+  return dataTileLayer;
+}
 
-//Initialize map and set attributes
-var map = new Map({
-  target: 'map',
-  controls: defaultControls().extend([new FullScreen()]),
-  layers: [backgroundLayer,
-  dataTileLayer],
-  view: new View({
-    maxZoom: 18,
-    minZoom: 1,
-    center: [-8018982.89, 4035116.36],
-    zoom: 6,
-  }),
+//initialize map with background, noaa data tile layer and starting coordinates
+function initialize_map(dataTileLayer){
+  let map = new Map({
+    target: 'map',
+    controls: defaultControls().extend([new FullScreen()]),
+    layers: [backgroundLayer,
+    dataTileLayer],
+    view: new View({
+      maxZoom: 18,
+      minZoom: 1,
+      center: [-8018982.89, 4035116.36],
+      zoom: 6,
+    }),
+  });
+  return map;
+}
 
-});
 //Displays Zoom level for debugging
-var zoomLevel = map.getView().getZoom();
+function display_zoom_level(map){
+  let zoomLevel = map.getView().getZoom();
 
-var zoomLevelElement = document.getElementById('zoomLevel');
-zoomLevelElement.innerHTML = 'Zoom level: ' + zoomLevel;
+  let zoomLevelElement = document.getElementById('zoomLevel');
+  zoomLevelElement.innerHTML = 'Zoom level: ' + zoomLevel;
 
-map.getView().on('change:resolution', function() {
-  var zoomLevel = map.getView().getZoom();
-  var zoomLevelElement = document.getElementById('zoomLevel');
-  zoomLevelElement.innerHTML = 'Zoom level: ' + Math.round(zoomLevel*10)/10;;
-});
+  map.getView().on('change:resolution', function() {
+    let zoomLevel = map.getView().getZoom();
+    let zoomLevelElement = document.getElementById('zoomLevel');
+    zoomLevelElement.innerHTML = 'Zoom level: ' + Math.round(zoomLevel*10)/10;;
+  });
+}
 
 //Display Z/X/Y of mouse location
-map.on("pointermove", function (evt) {
-  var tileCoord = tileGrid.getTileCoordForCoordAndZ(evt.coordinate, map.getView().getZoom());
-  var tileLocation = "Tile location: " + tileCoord[0] + "/" + tileCoord[1] + "/" + tileCoord[2];
-  document.getElementById("tile-location").innerHTML = tileLocation;
-});
-})
-
-
-
+function display_mouse_xyz(map){
+  map.on("pointermove", function (evt) {
+    let tileCoord = tileGrid.getTileCoordForCoordAndZ(evt.coordinate, map.getView().getZoom());
+    let tileLocation = "Tile location: " + tileCoord[0] + "/" + tileCoord[1] + "/" + tileCoord[2];
+    document.getElementById("tile-location").innerHTML = tileLocation;
+  });
+}
