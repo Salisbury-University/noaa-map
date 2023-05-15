9950e0db53742d3d757a81b3e7d825e0f18d3e2e
idugan100 <isaacd4444@gmail.com>
2023-05-14 20:30:58 -0400

    consolidating the migrations so they will run properly to get tokens working again

--- tile_backend/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ tile_backend/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('name');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
+            $table->integer('uses')->default(0);
+            $table->integer('max_usage')->default(100);
             $table->timestamp('last_used_at')->nullable();
             $table->timestamp('expires_at')->nullable();
             $table->timestamps();
