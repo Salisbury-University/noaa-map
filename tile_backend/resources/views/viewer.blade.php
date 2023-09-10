d9a899874b614afc88c1e523e38269d0212f5ca0
idugan100 <isaacd4444@gmail.com>
2023-09-09 21:39:25 -0400

    shrunk all logo images so they don't take up the half the sceen. Also removed the link from the viewer logo

--- tile_backend/resources/views/viewer.blade.php
+++ tile_backend/resources/views/viewer.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-      <a href="https://www.bathmap.net/" class="d-flex justify-content-center">
-        <img src="{{asset("./logo.png")}}" alt="Logo" style="width:20em;height:20em;float:right" >
-      </a>
+      <div class="d-flex justify-content-center">
+        <img src="{{asset("./logo.png")}}" alt="Logo" style="width:15em;height:14em;float:right"  class="mt-2" >
+      </div>
       <div class="d-flex justify-content-center">
           <select name="gridID" id="grid_selector">
             <option value="" selected >All Locations</option>
