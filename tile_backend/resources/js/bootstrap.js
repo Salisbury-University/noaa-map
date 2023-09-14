eec2106b712c322fdf84a6e79d1239f3a290cf66
darkfutrue <bpb9629@gmail.com>
2023-09-14 09:34:18 -0400

    Modified welcome_blade to add slideshow with bootstrap carousel

--- tile_backend/resources/js/bootstrap.js
+++ tile_backend/resources/js/bootstrap.js
@@ -11,6 +11,19 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+$("#myCarousel").carousel();
+
+$(".item").click(function(){
+    $("#myCarousel").carousel(1);
+});
+
+$(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+});
+
+$(".right").click(function(){
+    $("#myCarousel").carousel("next");
+});
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
