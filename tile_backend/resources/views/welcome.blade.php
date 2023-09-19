6db42d7203a8ed97beed84c773595bbfbdedc221
jmeyers5 <jmeyers5@gulls.salisbury.edu>
2023-09-19 09:38:46 -0400

    new slideshow

--- tile_backend/resources/views/welcome.blade.php
+++ tile_backend/resources/views/welcome.blade.php
@@ -1,37 +1,220 @@
 @extends('layouts.app')
 
+
 @section('content')
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Bathymetric Tile API</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        
+
+    </head>
+    <body class="antialiased">
+        <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-blue-500 selection:text-white">
+            {{-- @if (Route::has('login'))
+                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+                    @auth
+                        <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-3 focus:rounded-sm focus:outline-blue-500">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-blue-500">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-blue-500">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif --}}
+
+            
+            <div class="container">
+                <div >
+                <style>
+* {box-sizing: border-box}
+body {font-family: Verdana, sans-serif; margin:0}
+.mySlides {}
+img {vertical-align: middle;}
+
+/* Slideshow container */
+.slideshow-container {
+  max-width: 300px;
+  position: relative;
+  margin: auto;
+}
+
+/* Next & previous buttons */
+.prev, .next {
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  width: auto;
+  padding: 16px;
+  margin-top: -22px;
+  color: black;
+  font-weight: bold;
+  font-size: 18px;
+  transition: 0.6s ease;
+  border-radius: 0 3px 3px 0;
+  user-select: none;
+}
+
+/* Position the "next button" to the right */
+.next {
+  right: 0;
+  border-radius: 3px 0 0 3px;
+}
+
+/* On hover, add a black background color with a little bit see-through */
+.prev:hover, .next:hover {
+  background-color: rgba(0,0,0,0.8);
+}
+
+/* Caption text */
+.text {
+  color: #f2f2f2;
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+}
+
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+
+/* The dots/bullets/indicators */
+.dot {
+  cursor: pointer;
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.6s ease;
+}
+
+.active, .dot:hover {
+  background-color: #717171;
+}
+
+/* Fading animation */
+.fade {
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
+
+@keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+/* On smaller screens, decrease text size */
+@media only screen and (max-width: 300px) {
+  .prev, .next,.text {font-size: 11px}
+}
+
+</style>
+
+
+<div class="slideshow-container">
+
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="./logo.png" style="width:100%">
+  <div class="text">Caption Text</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="" alt="testing"style="width:100%">
+  <div class="text">Caption Two</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="" alt="lmao"style="width:100%">
+  <div class="text">Caption Three</div>
+</div>
+
+<a class="prev" onclick="plusSlides(-1)">❮</a>
+<a class="next" onclick="plusSlides(1)">❯</a>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+</div>
+
+<script>
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+</script>
+                <!--link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <style>
   .carousel-inner > .item > img,
   .carousel-inner > .item > a > img {
-    width: 70%;
+    width: 20%;
+    height: 25%;
     margin: auto;
   }
   </style>
-</head>
-<body>
+
 
 <div class="container">
   <h2>Activate Carousel with JavaScript</h2>
-  <div id="myCarousel" class="carousel slide">
+  <div id="myCarousel" class="carousel slide"-->
     <!-- Indicators -->
-    <ol class="carousel-indicators">
+    <!--ol class="carousel-indicators">
       <li class="item1 active"></li>
       <li class="item2"></li>
       <li class="item3"></li>
-    </ol>
+    </ol-->
 
     <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox">
+    <!--div class="carousel-inner" role="listbox">
 
       <div class="item active">
         <img src="./logo.png" alt="Chania" width="460" height="345">
@@ -57,10 +240,10 @@
         </div>
       </div>
 
-    </div>
+    </div-->
 
     <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <!--a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
       <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
       <span class="sr-only">Previous</span>
     </a>
@@ -98,8 +281,69 @@ $(document).ready(function(){
     $("#myCarousel").carousel("next");
   });
 });
-</script>
+</script-->
+                </div>
+                <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet">
+                
+                <div class = container>
+                    <div class = "row py-2">
+                        <div class="d-grid gap-2">
+                            <a href="/viewer" class="btn btn-primary btn-rounded btn-block" role="button">
+                                
+                                <h1 class="display-4">See the maps</h1>
+                                <p class="lead">Take a first hand look at the bathymetric data with our interactive map viewer built on OpenLayers.</p>
+                            </a>
+                        </div>        
+                    </div>
+                    <div class = "row py-2">
+                        <div class="col-sm-6">
+                            <a href="/tutorial" class="btn btn-primary btn-rounded" role="button">
 
-</body>
+                                <h1 class="display-4">Tutorials</h1>
+                                <p class="lead">The BathMap API tutorials will guide you through the process of using these resources for yourself! </p>
+                            </a>
+                        </div>  
+                        <div class="col-sm-6">
+                            <a href="/documentation" class="btn btn-primary btn-rounded" role="button">
+                                 
+                                <h1 class="display-4">Documentation</h1>
+                                <p class="lead">The BathMap API has wonderful documentation covering every aspect of our product. Useful for someone brand new or experianced!</p>
+                            </a>
+                        </div>  
+                    </div>
+                    <div class = "row py-2">
+                        <div class="col-sm-6">
+                            <a href="/about" class="btn btn-primary btn-rounded" role="button">
+
+                                <h1 class="display-4">About</h1>
+                                <p class="lead">Learn more about the team behind this project! Learn more about bathymetric mapping and the worlds oceans!</p>
+                            </a>
+                        </div>  
+                        <div class="col-sm-6">
+                            <a href="https://github.com/Salisbury-University/noaa-map" class="btn btn-primary btn-rounded" role="button">
+                                 
+                                <h1 class="display-4">Open Source</h1>
+                                <p class="lead">At BathMap we belive in free and open source software! You can find us on github by clicking here!</p>
+                            </a>
+                        </div>  
+                    </div>
+
+                </div>
+                
+                
+                <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">
+                    <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-left">
+                        <div class="flex items-center gap-4">
+                           
+                        </div>
+                    </div>
+
+                  
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
+
 @endsection
