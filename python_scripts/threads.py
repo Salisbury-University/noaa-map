06b324ab957e8562c79b6d12b61d19acb093e43e
MarkSchwe <markdanielschweitzer@gmail.com>
2023-09-28 10:21:20 -0400

    most current version of migration program

--- python_scripts/threads.py
+++ python_scripts/threads.py
@@ -10,20 +10,22 @@ import mysql.connector
 import ctypes
 import threading
 
+indexGL = 0
 
-def implement(lock,arr,size,index):
+def implement(lock,arr,size):
     mSQL2 = mysql.connector.connect(host = "bathmap-db-1.cneazldeoyra.us-east-1.rds.amazonaws.com",username = "admin",password = "towMater",database = "bathmap_mysql_1", connect_timeout=6000)
     SQLCursor2 = mSQL2.cursor()
-    while(index.value < size):
+    global indexGL
+    while(indexGL < size):
         lock.acquire()
         try:
-            if index.value == size:
+            if indexGL == size:
                 break
             #z, row, column, gID, tileID
-            SQLCursor2.executemany("insert into tile values (%s,%s,%s);",arr[index.value])
+            SQLCursor2.executemany("insert into tile values (%s,%s,%s);",arr[indexGL])
             mSQL2.commit()
-            index.value = index.value + 1
-            print("tile chunks remaining: ", size-index.value)
+            indexGL = indexGL + 1
+            print("tile chunks remaining: ", size-indexGL)
         finally:
             lock.release()
 
@@ -61,7 +63,7 @@ if __name__ == "__main__":
     cursor.execute("select count(*) from map")
     row2 = cursor.fetchall()
     
-    
+    ti1 = time.time()
     fleems = []
     index = 0
     for x in row:
@@ -73,15 +75,8 @@ if __name__ == "__main__":
     t1 = threading.Thread(target=implement2, args=(fleems,))
     t1.start()
     t1.join()
-    """
-    ti1 = time.time()
 
-
-    ti2 = time.time()
-    print(ti2-ti1)
     print("Location migration finished")
-    """
-    """
     
     fleems2 = []
     cursor.execute("select * from images")
@@ -95,7 +90,7 @@ if __name__ == "__main__":
     print(len(fleems2))
     for l in range(0,len(fleems2)):
         data2.append(fleems2[l])
-        if l % 5000 == 0 and l != 0:
+        if l % 3500 == 0 and l != 0: #ADJUST % VALUE BASED ON SIZE OF IMAGES. OTHERWISE INCREASE MAXIMUM BIT SIZE OF SERVER
             fleems3.append(data2)
             print(len(data2))
             data2 = []
@@ -106,11 +101,11 @@ if __name__ == "__main__":
     lock = multiprocessing.Lock() 
     proc = []
     index = 0   
-    
-    ind = 0
-    ti1 = time.time()
+    lock2 = threading.Lock()
+
+    ind = 1
     for i in range(0,6):
-        t1 = threading.Thread(target=implement, args=(fleems3,len(fleems3),ind))
+        t1 = threading.Thread(target=implement, args=(lock2,fleems3,len(fleems3)))
         t1.start()
         proc.append(t1)
 
@@ -118,15 +113,13 @@ if __name__ == "__main__":
         pr.join()
     
     print("image migration finished")
-    ti2 = time.time()
     
-    print(ti2-ti1)
-    
-
     cursor.execute('select value from metadata where name = "bounds" or name = "center"')
     rows4 = cursor.fetchall()
     data5 = (inp, rows4[0][0], rows4[1][0])
     SQLCursor.execute('insert into grid values (%s, %s, %s)',data5)
-
+    mSQL.commit()
+    ti2 = time.time()
+    print(ti2-ti1)
     print("migration complete")        
-    """
+    
