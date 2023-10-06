c239262865c53ebc41de14d0a6b005fc72d03088
Isaac Dugan <isaacdugan@Isaacs-MacBook-Pro.local>
2023-10-05 22:52:32 -0400

    grid ids names and coordinates in documentaton page are now pulled from db not hard coded

--- tile_backend/resources/views/informational/documentation.blade.php
+++ tile_backend/resources/views/informational/documentation.blade.php
@@ -43,135 +43,18 @@
                                                 <thead>
                                                   <tr>
                                                     <th scope="col">Region</th>
-                                                    <th scope="col">Grid ID</th>
+                                                    <th >Grid ID</th>
+                                                    <th> Grid Center</th>
                                                   </tr>
                                                 </thead>
                                                 <tbody>
-                                                  <tr>
-                                                    <th scope="row">Vermont</th>
-                                                    <td>01a</td>
-                                                  </tr>
-                                                  <tr>
-                                                    <th scope="row">Maine</th>
-                                                    <td>01</td>
-                                                  </tr>
-                                                  <tr>
-                                                    <th scope="row">New York State</th>
-                                                    <td colspan="2">02a</td>
-                                                  </tr>
-                                                  <tr>
-                                                    <th scope="row">Boston</th>
-                                                    <td colspan="2">02b</td>
-                                                  </tr>
-                                                  <tr>
-                                                    <th scope="row">New York City</th>
-                                                    <td colspan="2">03</td>
-                                                  </tr>
-                                                  <tr>
-                                                    <th scope="row">Eastern Maryland</th>
-                                                    <td colspan="2">04</td>
-                                                  </tr>
-                                                  <tr>
-                                                    <th scope="row">Virginia</th>
-                                                    <td colspan="2">05</td>
-                                                  </tr>
-                                                  <tr>
-                                                    <th scope="row">Kitty Hawk</th>
-                                                    <td colspan="2">06</td>
-                                                  </tr>
-                                                  <tr>
-                                                    <th scope="row">Charlotte</th>
-                                                    <td colspan="2">07</td>
-                                                  </tr>
-                                                  <tr>
-                                                    <th scope="row">Myrtle Beach</th>
-                                                    <td colspan="2">08</td>
-                                                  </tr>
-                                                  <tr>
-                                                    <th scope="row">Jacksonville</th>
-                                                    <td colspan="2">09</td>
-                                                  </tr>                                        
-                                                  <tr>
-                                                    <th scope="row">Bahamas</th>
-                                                    <td colspan="2">10</td>
-                                                  </tr>                                       
-                                                   <tr>
-                                                    <th scope="row">Dominican Republic</th>
-                                                    <td colspan="2">11</td>
-                                                  </tr>                                       
+                                                  @foreach ($grids as $grid)
                                                     <tr>
-                                                    <th scope="row">Cancun</th>
-                                                    <td colspan="2">12</td>
-                                                  </tr>                                        
-                                                  <tr>
-                                                    <th scope="row">West Florida</th>
-                                                    <td colspan="2">13</td>
-                                                  </tr>                                        
-                                                  <tr>
-                                                    <th scope="row">New Orleans</th>
-                                                    <td colspan="2">14</td>
-                                                  </tr>
-                                                  <tr>
-                                                    <th scope="row">Gulf of Mexico</th>
-                                                    <td colspan="2">15</td>
-                                                  </tr>
-                                                  <tr>
-                                                    <th scope="row">Lake Erie</th>
-                                                    <td colspan="2">16</td>
-                                                  </tr>
-                                                  <tr>
-                                                    <th scope="row">Lake Michiagan</th>
-                                                    <td colspan="2">17</td>
-                                                  </tr>
-                                                  <tr>
-                                                    <th scope="row">Lake Superior</th>
-                                                    <td colspan="2">18</td>
-                                                  </tr>
-                                                  <tr>
-                                                    <th scope="row">Lake Huron</th>
-                                                    <td colspan="2">19</td>
-                                                  </tr>
-                                                  <tr>
-                                                    <th scope="row">California</th>
-                                                    <td colspan="2">20</td>
-                                                  </tr>
-                                                  <tr>
-                                                    <th scope="row">Washington State</th>
-                                                    <td colspan="2">21</td>
-                                                  </tr>
-                                                  <tr>
-                                                    <th scope="row">Western Canada</th>
-                                                    <td colspan="2">22</td>
-                                                  </tr>                                    
-                                                 <tr>
-                                                    <th scope="row">Gulf of Alaska</th>
-                                                    <td colspan="2">23</td>
-                                                  </tr>
-                                                </tr>                                     
-                                                <tr>
-                                                    <th scope="row">Beaufort Sea</th>
-                                                    <td colspan="2">24</td>
-                                                </tr>
-                                                <tr>
-                                                    <th scope="row">Bering Sea</th>
-                                                    <td colspan="2">25</td>
-                                                </tr>
-                                                <tr>
-                                                    <th scope="row">North Pacific Ocean</th>
-                                                    <td colspan="2">26</td>
-                                                </tr>
-                                                <tr>
-                                                    <th scope="row">Anchorage</th>
-                                                    <td colspan="2">27</td>
-                                                </tr>
-                                                <tr>
-                                                    <th scope="row">Hawaii</th>
-                                                    <td colspan="2">28</td>
-                                                </tr>
-                                                <tr>
-                                                    <th scope="row">East China Sea</th>
-                                                    <td colspan="2">29</td>
-                                                </tr>
+                                                      <th scope="row">{{$grid->name}}</th>
+                                                      <td>{{$grid->gridID}}</td>
+                                                      <td>{{$grid->gridCenter}}</td>
+                                                    </tr>
+                                                  @endforeach
                                                 </tbody>
                                               </table>
                                         </div>
