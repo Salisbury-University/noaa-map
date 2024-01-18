7720221aef0d76a753b766c50e49a6aa1562eb70
idugan100 <isaacd4444@gmail.com>
2024-01-17 22:19:35 -0500

    moving js and css to proper places to declutter file

--- tile_backend/resources/views/welcome.blade.php
+++ tile_backend/resources/views/welcome.blade.php
@@ -14,266 +14,53 @@
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        
-<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
         <script type="text/javascript" src="css3-mediaqueries.js"></script>
-        
-
     </head>
 
     <body class="antialiased">
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-blue-500 selection:text-white">
-            {{-- @if (Route::has('login'))
-                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
-                    @auth
-                        <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-3 focus:rounded-sm focus:outline-blue-500">Dashboard</a>
-                    @else
-                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-blue-500">Log in</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-blue-500">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif --}}
-
-            
             <div class="container">
-                <div >
-                <style>
-* {box-sizing: border-box}
-.mySlides {display: none}
-img {vertical-align: middle;}
+                <div class="slideshow-container">
 
-/* Slideshow container */
-.slideshow-container {
-  max-width: 300px;
-  position: relative;
-  margin: auto;
-}
+                <div class="mySlides fade">
+                  <a a href="https://github.com/Salisbury-University/noaa-map">
+                    <img src="./openSourceSea.jpg" style="width:100%;height:15em">
+                  </a>
+                  <div class="text2 text-black"><strong>Open source like the seas</strong></div>
+                </div>
 
-/* Next & previous buttons */
-.prev, .next {
-  cursor: pointer;
-  position: absolute;
-  top: 50%;
-  width: auto;
-  padding: 16px;
-  margin-top: -22px;
-  color: black;
-  font-weight: bold;
-  font-size: 18px;
-  transition: 0.6s ease;
-  border-radius: 0 3px 3px 0;
-  user-select: none;
-}
+                <div class="mySlides fade">
+                  <a a href="{{ url('/about') }}">
+                    <img src="./teamPhoto426.JPG" alt="team photo"style="width:100%;height:15em">
+                  </a>
+                  <div class="text text-black "><strong>Our team</strong></div>
+                </div>
 
-/* Position the "next button" to the right */
-.next {
-  right: 0;
-  border-radius: 3px 0 0 3px;
-}
-
-/* On hover, add a black background color with a little bit see-through */
-.prev:hover, .next:hover {
-  background-color: rgba(0,0,0,0.8);
-}
-
-/* Caption text */
-.text {
-  color: #f2f2f2;
-  font-size: 20px;
-  background-clip: padding-box;
-  position: absolute;
-  bottom: 8px;
-  left: 100px;
-  width: 100px;
-  text-align: center;
-  background: rgb(189, 195, 199); /* Fallback */
-  background: rgba(189, 195, 199, 0.5);
-  background-origin: 30px;
-  border-radius: 5px;
-
-}
-.text2 {
-  color: #f2f2f2;
-  font-size: 20px;
-  background-clip: padding-box;
-  position: absolute;
-  bottom: 8px;
-  left: 25px;
-  width: 250px;
-  text-align: center;
-  background: rgb(189, 195, 199); /* Fallback */
-  background: rgba(189, 195, 199, 0.65);
-  background-origin: 30px;
-  border-radius: 5px;
-}
-.text3 {
-  color: #f2f2f2;
-  font-size: 20px;
-  background-clip: padding-box;
-  position: absolute;
-  bottom: 8px;
-  left: 100px;
-  width: 100px;
-  text-align: center;
-  background: rgb(189, 195, 199); /* Fallback */
-  background: rgba(189, 195, 199, 0.85);
-  background-origin: 30px;
-  border-radius: 5px;
-}
-
-/* Number text (1/3 etc) */
-.numbertext {
-  color: #f2f2f2;
-  font-size: 12px;
-  padding: 8px 12px;
-  position: absolute;
-  top: 0;
-}
-
-/* The dots/bullets/indicators */
-.dot {
-  cursor: pointer;
-  height: 15px;
-  width: 15px;
-  margin: 0 2px;
-  background-color: #bbb;
-  border-radius: 50%;
-  display: inline-block;
-  transition: background-color 0.6s ease;
-}
-
-.active, .dot:hover {
-  background-color: #717171;
-}
-
-/* Fading animation */
-.fade {
-    -webkit-animation-name: fade;
-  -webkit-animation-duration: 1.5s;
-  -webkit-animation-fill-mode: forwards; 
-  animation-name: fade;
-  animation-duration: 1.5s;
-  animation-fill-mode: forwards; 
-}
-
-@keyframes fade {
-  from {opacity: .4} 
-  to {opacity: 1}
-}
-
-/* On smaller screens, decrease text size */
-@media only screen and (max-width: 900px) {
-  .prev, .next,.text {font-size: 11px}
-  .slideshow-container {max-width: 225px;}
-  .dot{height: 10px;
-  width:10px;}
-}
-
-</style>
-
-<div class="slideshow-container">
-
-<div class="mySlides fade">
-  <a a href="https://github.com/Salisbury-University/noaa-map">
-    <img src="./openSourceSea.jpg" style="width:100%;height:15em">
-  </a>
-  <div class="text2 text-black"><strong>Open source like the seas</strong></div>
-</div>
-
-<div class="mySlides fade">
-  <a a href="{{ url('/about') }}">
-    <img src="./teamPhoto426.JPG" alt="team photo"style="width:100%;height:15em">
-  </a>
-  <div class="text text-black "><strong>Our team</strong></div>
-</div>
-
-<div class="mySlides fade">
-  <a a href="{{ url('/viewer') }}">
-    <img src="./mapViewerScreen1.png" alt="viewer photo"style="width:100%;height:15em">
-  </a>
-  <div class="text3 text-black"><strong>The maps</strong></div>
-</div>
+                <div class="mySlides fade">
+                  <a a href="{{ url('/viewer') }}">
+                    <img src="./mapViewerScreen1.png" alt="viewer photo"style="width:100%;height:15em">
+                  </a>
+                  <div class="text3 text-black"><strong>The maps</strong></div>
+                </div>
 
 
-<a class="prev" onclick="plusSlides(-1)">❮</a>
-<a class="next" onclick="plusSlides(1)">❯</a>
+                <a class="prev" onclick="plusSlides(-1)">❮</a>
+                <a class="next" onclick="plusSlides(1)">❯</a>
 
-</div>
-<br>
+              </div>
+                <br>
 
-<div style="text-align:center">
-  <span class="dot" onclick="currentSlide(1)"></span> 
-  <span class="dot" onclick="currentSlide(2)"></span> 
-  <span class="dot" onclick="currentSlide(3)"></span> 
-</div>
+                <div style="text-align:center">
+                  <span class="dot" onclick="currentSlide(1)"></span> 
+                  <span class="dot" onclick="currentSlide(2)"></span> 
+                  <span class="dot" onclick="currentSlide(3)"></span> 
+                </div>
 
-<script>
-let slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+                </div>
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = "dot";
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
-</script>
-</div>
-
-<style>
-.btn-primary, .btn-primary:active, .btn-primary:visited {
-    background-color: #f7f7f7 !important;
-    box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
-    border-color: #f7f7f7;
-    @media (max-width: 100px){
-      background-color: black;
-    }
-}
-
-.btn-primary:hover {
-    box-shadow: 6px 6px 10px rgba(0, 0, 0, 0.2);
-    border-color: #f7f7f7;
-}
-
-h1{
-    color: #005eae;
-}
-
-p{
-    color: #1aa7d8;
-}
-@media screen and (max-width: 767px){
-  .col-sm-6{padding: 2px}
-  .btn-primary{
-    height: 100px;
-    
-  }
-  .lead{font-size: small;}
-  .display-4{font-size: medium;}
-}
-
-</style>
-<!-- Container for the homepage buttons -->
                 <div class = "container" >
                     <div class = "row py-2">
                         <div class="d-grid gap-2">
@@ -318,5 +105,32 @@ p{
         </div>
     </body>
 </html>
-
+<script>
+  let slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = "dot";
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+  }
+  </script>
 @endsection
